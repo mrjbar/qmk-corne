e6037d4b876d531d5513a917bf4f1a33c290d684
mrjbar <jabari.lofton@sjsu.edu>
2023-02-23 09:10:35 -0800

    Added qmk corne keymap

---
+++ config.h
@@ -0,0 +1,14 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#undef TAPPING_TERM
+#define TAPPING_TERM 176
+#define AUTO_SHIFT_TIMEOUT 165
+#define NO_AUTO_SHIFT_TAB
+#define NO_AUTO_SHIFT_ALPHA
+#define NO_AUTO_SHIFT_NUMERIC
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_TOGGLE 2
+#define ONESHOT_TIMEOUT 3000
+#define COMBO_COUNT 3
---
+++ rules.mk
@@ -0,0 +1,11 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+BOOTLOADER = atmel-dfu
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+AUTO_SHIFT_ENABLE = yes
+MOUSEKEY_ENABLE = no
+TAP_DANCE_ENABLE = yes
+SPACE_CADET_ENABLE = yes
+COMBO_ENABLE = yes
+CAPS_WORD_ENABLE = yes
